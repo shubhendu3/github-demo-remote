b25876948eac208825b66384b19f70aa19a938bc
shubhendu3 <63185422+shubhendu3@users.noreply.github.com>
2021-09-18 19:10:34 +0530

    Initial commit of math.py

--- math.py
+++ math.py
@@ -1,19 +1,11 @@
-#Add
 def add(x,y):
-    return x+y
-#Subtract
-def subtract(x,y):   #on master branch
-    return x-y
-#Multiply
-def multiply(x,y):   #on bug2 branch
-    return x*y
-#Divide
-def divide(x,y):     #on Bug3 branch
-    if y == 0:
-	return divide by zero error
-    else:
-	return x/y
+	pass
 
-# Square implementation
-def square(x):
-    return x*x
+def subtract(x,y):   
+	pass
+
+def multiply(x,y):  
+	pass
+
+def divide(x,y):   
+	pass
